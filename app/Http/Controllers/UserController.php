da377d33b4b5b36d26964b954057628c6beb21b2
Enggarrahayu <enggarrahayu64@gmail.com>
2022-12-18 05:14:13 +0700

    add register user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use GrahamCampbell\ResultType\Success;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Routing\Controller;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+class UserController extends Controller
+{
+    use ValidatesRequests;
+    public function getSignUp()
+    {
+        return view('register');
+    }
+    public function postSignUp(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email|unique:users',
+            'name' => 'required|max:30|unique:users',
+            'password' => 'required|min:4'
+        ]);
+
+        $email = $request->email;
+        $name = $request->name;
+        $password = bcrypt($request->password);
+
+        $user = new User();
+        $user->email = $email;
+        $user->name = $name;
+        $user->password = $password;
+
+        $user->save();
+        return view('welcome');
+    }
+}
